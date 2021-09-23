2ee4df2e0688379b06beb74fb53ef715bb43f1a2
Fernando <fernando.viana.nt@gmail.com>
2021-09-22 21:42:49 -0300

    data refactor

--- src/data/data.js
+++ src/data/data.js
@@ -20,7 +20,7 @@ export const data = {
       price: 450.00,
       link: "/destino",
       categoria: 'individual',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: "A pouco mais de 10 km da Praia do Forte, a vila de Imbassaí teve origem em uma pequena aldeia indígena que hoje se tornou um dos destinos turísticos mais gostosos da Bahia. Com ótima infraestrutura de pousadas, bares, restaurantes, lojas de artesanato, e opções para todos os gostos, a Praia de Imbassaí vem conquistando todos que passam alguns dias por lá. Principalmente por causa dos ótimos preços praticados, bem diferente da sua badalada vizinha."
     },
     {
       id: 3,
@@ -72,11 +72,10 @@ export const data = {
       price1: 450.00,
       price2: 750.00,
       price3: 120.00,
-      link: "/destino",
       categoria1: 'A partir de 3 pessoas',
       categoria2: '4 a 6 pessoas',
       categoria3: 'Acima de 8 pessoas',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: "Com saída dos hotéis de Salvador, seguiremos a Linha Verde para conhecer duas das mais belas praias do Litoral Baiano. Primeira parada na Praia do Forte - conhecida como a Polinésia Brasileira, você terá a oportunidade de conhecer o Projeto Tamar, reconhecido internacionalmente como uma das mais bem sucedidas experiências de conservação marinha. Visitará a vilinha mais charmosa  com um ambiente rústico e aconchegante. Segunda parada na Praia de Guarajuba - Uma praia com selo Bandeira Azul que reserva as melhores piscinas naturais, um balneário bem estruturado com águas tranquilas e cristalinas que compõe uma paisagem fantástica. Areia bem fofinha para caminhar pelos corais e uma imensidão de coqueiros."
     },
     {
       id: 8,
@@ -86,21 +85,21 @@ export const data = {
       price1: 450.00,
       price2: 700.00,
       price3: 120.00,
-      link: "/destino",
       categoria1: 'A partir de 3 pessoas',
       categoria2: '4 a 6 pessoas',
       categoria3: 'Acima de 8 pessoas',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: "A pouco mais de 10 km da Praia do Forte, a vila de Imbassaí teve origem em uma pequena aldeia indígena que hoje se tornou um dos destinos turísticos mais gostosos da Bahia. Com ótima infraestrutura de pousadas, bares, restaurantes, lojas de artesanato, e opções para todos os gostos, a Praia de Imbassaí vem conquistando todos que passam alguns dias por lá. Principalmente por causa dos ótimos preços praticados, bem diferente da sua badalada vizinha."
     },
     {
       id: 9,
       imgLink: "https://res.cloudinary.com/dh84pxwgu/image/upload/v1628200266/WhatsApp_Image_2021-08-05_at_18.50.54_jf00rc.jpg",
       title: "Passeio de Lancha",
       subtitle: "A partir de ",
-      price1: 700.00,
-      price2: 450.00,
-      price3: 120.00,
-      link: "/destino",
+      prices: [
+        700.00,
+        450.00,
+        120.00
+      ],
       categoria1: 'A partir de 9 pessoas',
       categoria2: 'Até 10 pessoas',
       categoria3: 'Até 11 pessoas',
@@ -114,11 +113,27 @@ export const data = {
       price1: 70.00,
       price2: 130.00,
       price3: 170.00,
-      link: "/destino",
       categoria1: 'Passeio',
       categoria2: 'Passeio + Translado ida e volta',
       categoria3: 'Passeio + Translado ida e volta + Almoço ',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: `Com saída do Terminal Marítimo( Atrás do Mercado Modelo), você irá embarcar   em escuna equipada com serviços de bar e guia acompanhante. Durante o percurso, visita-se a Ilha dos Frades e Ponta de Areia (Itaparica), o passeio possibilita uma bela vista da cidade de Salvador. A partir da Baía de Todos os Santos, Forte São Marcelo, Elevador Lacerda e o Mercado Modelo.
+
+      ➡️Inclui: rodada de frutas tropicais, um guia e um grupo de samba
+
+      ➡️Embarcação: Escuna com capacidade para até 150 pessoas ( estamos embarcando apenas 50% do total)
+
+      🔵Duração são de 10 horas de passeio
+      🔵Saída do terminal marítimo
+      🔵 Horário de saída às 8:30
+      🔵 Primeira parada na Ilha dos Frades ( 2 horas )
+      🔵 Segunda parada  na Ilha de Itaparica ( 3 horas )
+      🔵 Almoço no restaurante Manguezal
+      🔵 Chegada em Salvador às 17:30
+      🔵 Aparelhos de mergulho são alugados no local
+
+      ➡️Ponto de partida
+
+      🚨Terminal Turístico Náutico da Bahia🚨`
     },
     {
       id: 11,
@@ -128,11 +143,10 @@ export const data = {
       price1: 250.00,
       price2: 400.00,
       price3: 80.00,
-      link: "/destino",
       categoria1: 'A partir de 3 pessoas',
       categoria2: '4 a 6 pessoas',
       categoria3: 'Acima de 8 pessoas',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: "Este passeio dura cerca de 5 horas e é um dos mais interessantes. Isso porque o guia irá te conduzir para uma viagem no tempo e te levará para conhecer cada cantinho dessa cidade fantástica. No percurso está incluso a Cidade Alta, o Largo do Pelourinho, Terreiro de Jesus, a Igreja de São Francisco (visita opcional), o Elevador Lacerda, a Igreja do Bonfim e o Mercado Modelo. Em todos os locais do roteiro haverá paradas para que você possa passear, tirar fotos e conhecer. É um passeio bem cultural e que vale a pena incluir em sua lista de o que fazer em Salvador."
     },
     {
       id: 12,
@@ -142,40 +156,30 @@ export const data = {
       price1: 700.00,
       price2: 450.00,
       price3: 120.00,
-      link: "/destino",
       categoria1: 'A partir de 3 pessoas',
       categoria2: '4 a 6 pessoas',
       categoria3: 'Acima de 8 pessoas',
-      description: "Lorem ipsum tortor ad fames magna pellentesque curabitur leo maecenas eros ligula, venenatis consequat fusce arcu bibendum platea eu primis nisi lacinia vitae, ullamcorper proin laoreet pretium erat lectus felis eu malesuada a. vulputate odio in ultrices dolor adipiscing placerat leo ac, phasellus justo porta et est curabitur lobortis ad faucibus, sedullamcorper ultrices etiam varius lobortis praesent fames. eu dictum in nibh iaculis facilisis suscipit odio cubilia fusce netus, duis odio cras donec lectus varius aenean lobortis nullam. lectus dapibus pharetra bibendum viverra congue dolor duis, vehicula cubilia faucibus sollicitudin consequat morbi, litora potenti risus sagittis fermentum donec."
+      description: `
+      Com saída dos hotéis de Salvador, seguiremos pelo extremo Norte do Estado até a divisa de Bahia e Sergipe por aproximadamente 200 km até chegar ao povado de Pontal e atravessar o Rio Real em lancha para Mangue Seco.
+
+      Mangue Seco é totalmente pé na areia e não existem ruas, a vila é composta por trilhas onde os 'bugueiros' conduzem os turistas para desfrutar da belíssima praia rodeada de coqueiros e dunas. Famosa por ter sido o cenário da novela Tieta, inspirada no romance de Jorge Amado, com potencial turístico enorme, pela beleza da região.
+
+      Com dunas de areia fininha, praia quase deserta, um cenário especial para um delicioso passeio de quadriciclo, banho de mar e aproveitar da deliciosa culinária.`
     },
   ],
   serviços: [
     {
+      id: 13,
       title: 'Transfer',
       smallDescription: 'Transporte entre o aeroporto e o seu destino de escolha, com confiabilidade e e segurança',
       iconLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1632173801/bus_120710_shasxs.svg',
-      id: 13,
-      imgLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1631383494/WhatsApp_Image_2021-09-11_at_15.04.19_fr08g1.jpg',
-      categoria1: '1 a 3 pessoas',
-      categoria2: '1 a 4 pessoas',
-      categoria3: 'Acima de 6 pessoas',
-      price1: 250,
-      price2: 400,
-      price3: 80,
+
     },
     {
+      id: 14,
       title: 'City Tour',
       smallDescription: 'Este passeio extremamente cultural, levará você por uma viagem no tempo, conhecendo o melhor da história de Salvador',
       iconLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1632174356/city_building_construction_cities_buildings_icon_133122_ibo2wy.svg',
-      description: 'Este passeio dura cerca de 5 horas e é um dos mais interessantes. Isso porque o guia irá te conduzir para uma viagem no tempo e te levará para conhecer cada cantinho dessa cidade fantástica. No percurso está incluso a Cidade Alta, o Largo do Pelourinho, Terreiro de Jesus, a Igreja de São Francisco (visita opcional), o Elevador Lacerda, a Igreja do Bonfim e o Mercado Modelo. Em todos os locais do roteiro haverá paradas para que você possa passear, tirar fotos e conhecer. É um passeio bem cultural e que vale a pena incluir em sua lista de o que fazer em Salvador',
-      id: 14,
-      imgLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1631383494/WhatsApp_Image_2021-09-11_at_15.04.19_fr08g1.jpg',
-      categoria1: '1 a 3 pessoas',
-      categoria2: '1 a 4 pessoas',
-      categoria3: 'Acima de 6 pessoas',
-      price1: 250,
-      price2: 400,
-      price3: 80,
     },
     {
       title: 'Pacotes',
@@ -195,14 +199,14 @@ export const data = {
       id: 16,
       imgLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1631383494/WhatsApp_Image_2021-09-11_at_15.04.19_fr08g1.jpg',
       title: 'Viaje e aproveite o momento com sua família',
-      smallDescription: 'Este passeio dura cerca de 5 horas e é um dos mais interessantes. Isso porque o guia irá te conduzir para uma viagem no tempo e te levará para conhecer cada cantinho dessa cidade fantástica.',
+      smallDescription: 'Uma experiência indescritível, onde você e sua família conhecerão o melhor da Bahia. Do farol da barra as praias paradisíacas do Litoral Norte, aqui temos de tudo: Relaxamento, diversão, cultura, culinária para agradar a todos os paladares. ',
       invert: false,
     },
     {
       id: 17,
       imgLink: 'https://res.cloudinary.com/dh84pxwgu/image/upload/v1628184380/cleverson-magalhaes-JfMZZqioMSU-unsplash_ej73lh.jpg',
       title: 'Conheça a cultura e a história da Bahia',
-      smallDescription: 'Este passeio dura cerca de 5 horas e é um dos mais interessantes. Isso porque o guia irá te conduzir para uma viagem no tempo e te levará para conhecer cada cantinho dessa cidade fantástica.',
+      smallDescription: 'Este passeio dura cerca de 5 horas e é um dos mais interessantes. Isso porque o guia irá te conduzir para uma viagem no tempo e te levará para conhecer cada cantinho dessa cidade fantástica. No percurso está incluso a Cidade Alta, o Largo do Pelourinho, Terreiro de Jesus, a Igreja de São Francisco (visita opcional), o Elevador Lacerda, a Igreja do Bonfim e o Mercado Modelo.',
       invert: true,
     }
   ]
