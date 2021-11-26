8a7c9595b2397524d52680610aac49dc667d3994
Fernando <fernando.viana.nt@gmail.com>
2021-11-26 11:03:56 -0300

    create testimony

--- src/pages/BackOffice/TestimonyAdminPage/index.jsx
+++ src/pages/BackOffice/TestimonyAdminPage/index.jsx
@@ -1,18 +1,30 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 
 import { FiEdit2 } from 'react-icons/fi';
-import { AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineDelete, AiFillBook } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import {
-  Container, DepoimentoContainer, Footer, TestimonysContainer, ButtonContainer,
+  Container, DepoimentoContainer, Footer, TestimonysContainer, ButtonContainer, Content, ButtonContainerHeader, FormContainer, Group,
 } from './styles';
 
 import { BackOfficeTemplate } from '../../../templates/BackOfficeTemplate';
 // import Loader from '../../../components/Loader';
+import { Form } from '../../../components/Form';
+import Input from '../../../components/Input';
+import { TextArea } from '../../../components/TextArea';
+import Button from '../../../components/Button';
+
+import isEmailValid from '../../../utils/isEmailValid';
 
 export const TestimonyAdminPage = () => {
   const [depoimentos, setDepoimentos] = useState([]);
+  const [registerTestimonyActive, setRegisterTestimonyActive] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [email, setEmail] = useState('');
+  const [testimony, setTestimony] = useState('');
+  const [date, setDate] = useState('');
+  const [name, setName] = useState('');
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     fetch('https://jctturismo.herokuapp.com/depoimentos')
@@ -22,30 +34,102 @@ export const TestimonyAdminPage = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (isEmailValid(email) && testimony.trim() !== '' && name.trim() !== '' && title.trim() !== '' && date.trim() !== '') {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [email, testimony, date, name, title]);
+
   return (
     <Container>
       <BackOfficeTemplate depoimentos>
-        <TestimonysContainer>
+        <Content>
           {
-            depoimentos.map((depoimento) => (
-              <DepoimentoContainer key={depoimento.key}>
-                <h3>{depoimento.title}</h3>
-                <i>{depoimento.testimony}</i>
-                <Footer>
-                  <small className="name">{depoimento.name}</small>
-                  <small>{depoimento.date}</small>
-                  <ButtonContainer>
-                    <Link to={`admin/editardepoimento/${depoimento.id}`}><FiEdit2 /></Link>
-                    <form action={`https://jctturismo.herokuapp.com/deletardepoimento/${depoimento.id}`} method="POST">
-                      <button type="submit" aria-label="deletar depoimento"><AiOutlineDelete /></button>
-                    </form>
-                  </ButtonContainer>
-                </Footer>
-              </DepoimentoContainer>
-            ))
-          }
-        </TestimonysContainer>
+            registerTestimonyActive ? (
+              <>
+                <ButtonContainerHeader>
+                  <h1>Cadastrar um novo depoimento</h1>
+                  <button type="button" onClick={() => setRegisterTestimonyActive(false)}>Voltar</button>
+                </ButtonContainerHeader>
+                <FormContainer>
+                  <Form action="https://jctturismo.herokuapp.com/criardepoimento" method="POST">
+                    <Input
+                      type="text"
+                      name="title"
+                      value={title}
+                      onChange={(event) => setTitle(event.target.value)}
+                      placeholder="Título"
+                    />
+                    <Input
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={(event) => setName(event.target.value)}
+                      placeholder="Nome"
+                    />
+                    <Group>
+                      <Input
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        placeholder="E-mail cadastral"
+                      />
+                      {
+                        !isEmailValid(email) && <small>Digite um e-mail válido</small>
+                      }
+                    </Group>
+                    <TextArea
+                      type="text"
+                      name="testimony"
+                      value={testimony}
+                      onChange={(event) => setTestimony(event.target.value)}
+                      placeholder="Depoimento"
+                    />
+                    <Input
+                      type="date"
+                      name="date"
+                      value={date}
+                      onChange={(event) => setDate(event.target.value)}
+                    />
+                    <Button type="submit" disabled={!formIsValid}>Cadastrar</Button>
+                  </Form>
+                </FormContainer>
+              </>
 
+            ) : (
+              <>
+                <ButtonContainerHeader>
+                  <h1>Depoimentos</h1>
+                  <button type="button" onClick={() => setRegisterTestimonyActive(true)}>Adicionar um novo depoimento <AiFillBook /></button>
+                </ButtonContainerHeader>
+                <TestimonysContainer>
+                  {
+                    depoimentos.map((depoimento) => (
+                      <DepoimentoContainer key={depoimento.key}>
+                        <h3>{depoimento.title}</h3>
+                        <i>{depoimento.testimony}</i>
+                        <Footer>
+                          <small className="name">{depoimento.name}</small>
+                          <small>{depoimento.date}</small>
+                          <ButtonContainer>
+                            <Link to={`admin/editardepoimento/${depoimento.id}`}><FiEdit2 /></Link>
+                            <form action={`https://jctturismo.herokuapp.com/deletardepoimento/${depoimento.id}`} method="POST">
+                              <button type="submit" aria-label="deletar depoimento"><AiOutlineDelete /></button>
+                            </form>
+                          </ButtonContainer>
+                        </Footer>
+                      </DepoimentoContainer>
+                    ))
+                  }
+                </TestimonysContainer>
+              </>
+            )
+          }
+
+        </Content>
       </BackOfficeTemplate>
     </Container>
   );
--- src/pages/BackOffice/TestimonyAdminPage/styles.js
+++ src/pages/BackOffice/TestimonyAdminPage/styles.js
@@ -42,6 +42,20 @@ export const Footer = styled.footer`
   `}
 `;
 
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    width: 100%;
+
+    h1 {
+      color: ${theme.colors.secundaryColor};
+      font-family: ${theme.font.family.texts};
+    }
+  `}
+`;
+
 export const ButtonContainer = styled.div`
   ${({ theme }) => css`
     display: flex;
@@ -55,6 +69,8 @@ export const ButtonContainer = styled.div`
       border:none;
       color: ${theme.colors.backgroundColor};
       font-size: 16px;
+
+      cursor: pointer;
     }
 
     a {
@@ -63,14 +79,66 @@ export const ButtonContainer = styled.div`
   `}
 `;
 
+export const ButtonContainerHeader = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: space-between;
+    min-height: 50px;
+    margin-top: 20px;
+    padding: 0px 20px;
+
+    button {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      justify-content: space-between;
+
+      border: 2px solid ${theme.colors.secundaryColor};
+      border-radius: 4px;
+
+      background: ${theme.colors.secundaryColor};
+      padding: 5px;
+
+      font-family: ${theme.font.family.texts};
+      font-size: 16px;
+      color: ${theme.colors.thirdColor};
+
+      cursor: pointer;
+
+      transition: ease-in-out all 300ms;
+
+      &:hover {
+        border: 2px solid ${theme.colors.secundaryColor};
+        color: ${theme.colors.secundaryColor};
+        background-color: ${theme.colors.thirdColor};
+      }
+    }
+  `}
+`;
+
 export const TestimonysContainer = styled.div`
   ${({ theme }) => css`
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     grid-gap: 10px;
 
     padding: 20px;
 
     font-family: ${theme.font.family.texts};
+  `}
+`;
+
+export const FormContainer = styled.div`
+  ${() => css`
+    margin: 0 auto;
+  `}
+`;
+
+export const Group = styled.div`
+  ${({ theme }) => css`
+    small {
+      color: red;
+      font-family: ${theme.font.family.texts};
+    }
   `}
 `;
